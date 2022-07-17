4740d2ab112d5fd69c725ccad4e5ce1d13d3786b
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-07-17 09:30:27 +0200

    fixing: adding encrypt to client register form

--- resources/views/auth/client/register.blade.php
+++ resources/views/auth/client/register.blade.php
@@ -10,7 +10,7 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('client.register') }}">
+        <form method="POST"  enctype="multipart/form-data" action="{{ route('client.register') }}">
             @csrf
 
             <!-- Name -->
