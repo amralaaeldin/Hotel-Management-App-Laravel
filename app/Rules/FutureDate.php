6744314ba54d5629968e13efdea5dd009db73749
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-09-24 12:54:05 +0200

    fix: fixing condition in FutureDate custom validation rule

--- app/Rules/FutureDate.php
+++ app/Rules/FutureDate.php
@@ -20,8 +20,8 @@ class FutureDate implements InvokableRule
     {
         // if (st_date in the past || st_date is today & request happens after 1:00 pm) 
         // fail
-
-        if ($value < date('Y-m-d') || ($value == date('Y-m-d') && Carbon::now('+02:00')->hour < 13)) {
+        
+        if ($value < date('Y-m-d') || ($value == date('Y-m-d') && Carbon::now('+02:00')->hour > 13)) {
             $fail('The :attribute must be valid future date. We support same day reservations until 01:00 PM.');
         }
     }
